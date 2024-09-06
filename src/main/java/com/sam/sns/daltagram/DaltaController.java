3e7138905bafd49eec76a3310e5893bf9378103c
Asm <gajaga2007@naver.com>
2024-09-06 23:35:16 +0900

    [안성민] - 회원가입 페이지 구성 - 2024/09/06

---
+++ src/main/java/com/sam/sns/daltagram/DaltaController.java
@@ -0,0 +1,16 @@
+package com.sam.sns.daltagram;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class DaltaController 
+{
+	@GetMapping("/join-view")
+	public String join()
+	{
+		return "user/join";
+	}
+}
